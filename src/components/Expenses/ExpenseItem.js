24b07f47d0bdb488cc37b0d0de1a70c09db92afe
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-07-11 20:49:28 +0530

    deletebtn

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,14 +5,21 @@ import Expenses from "../Expenses/Expenses";
 import Card from "../UI/Card";
 
 const ExpenseItem=(props)=> {
-
+  const ClickHandler=()=>{
+    alert("Clicked");
+  }
+  const deleteBtn = () => {
+     props.onDelete(props.id);
+  };
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id={props.id}>
        <ExpensesDate date={props.date}/>
        {/* <ExpenseDetails title={props.title} amount={props.amount} location={props.locationOfExpenditure}/> */}
-      <div className="expense-item__description"><h2> {props.title}</h2></div>
+      <div className="expense-item__description" ><h2> {props.title}</h2></div>
       <div className="expense-item__description"><h2> {props.locationOfExpenditure}</h2></div>
       <div className="expense-item__price">${props.amount}</div>
+      {/* <button onClick={ClickHandler}>Click here</button> */}
+      <button onClick={deleteBtn}>Delete Expense</button>
     </Card>
   );
 }
