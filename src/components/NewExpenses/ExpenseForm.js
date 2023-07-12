e2f1b4fe9679f8eca69874788142f395c2d031d1
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-07-12 16:24:12 +0530

    submit the expense

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -6,20 +6,58 @@ const ExpenseForm = () => {
     const [enterAmount,setEnterAmount]=useState('');
     const [enterLocation,setEnterLocation]=useState('');
     const [enterDate,setEnterDate]=useState('');
+    // const [enterInput,setInput]=useState({
+    //     enterTitle:'',
+    //     enterAmount:'',
+    //     enterLocation:'',
+    //     enterDate:''
+    // })
     const titleHandler=(event)=>{
         setEnterTitle(event.target.value);
+        //  setInput({
+        //     ...enterInput,
+        //     enterTitle:event.target.value
+        //  })
+        //it is a gud way because whenevre update more values in new version helpfull
+        // setInput((previouseState)=>{
+        //   return {...previouseState,
+        //     enterTitle:event.target.value}
+        // })
     }
     const amountHandler=(event)=>{
-         setEnterAmount(event.target.value);
+        setEnterAmount(event.target.value);
+        // setInput({
+        //     ...enterInput,
+        //     enterAmount:event.target.value
+        //  })
     }
     const locationHandler=(event)=>{
-         setEnterLocation(event.target.value);
+        setEnterLocation(event.target.value);
+        // setInput({
+        //     ...enterInput,
+        //     enterLocation:event.target.value
+        //  })
     }
     const dateHandler=(event)=>{
         setEnterDate(event.target.value);
+        // setInput({
+        //     ...enterInput,
+        //     enterDate:event.target.value
+        //  })
     }
+    const submitHandler=(event)=>{
+        event.preventDefault();
+        const ExpenseData={
+            title:enterTitle,
+            amount:enterAmount,
+            locationOfExpenditure:enterLocation,
+            date:new Date(enterDate)
+        }
+        console.log(ExpenseData);
+    }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__controls'>
           <label>Title</label>
